00c96ee367acf564713ef7a2a88bb9a1a8c7a630
liangjie93 <you@liang_jie1017@sina.com>
2016-08-24 19:53:07 +0800

    del

--- public/js/controllers/manager2.js
+++ public/js/controllers/manager2.js
@@ -1,15 +1,23 @@
 app.controller('manager2Ctrl', function ($scope,$http) {
-    //销售人员显示
+
+//销售人员显示
     $http.post("/crm/user/info/lists",{"start_page": 0,"page_size": 0})
     .success(function(response) {
         $scope.salesmans = response.data.list;
     });
-    
-
-    
-    //编辑
+//添加
+    $scope.$watch('new_name',function(){$scope.test2();});
+    $scope.$watch('new_phone',function(){$scope.test2();});
+    $scope.test2 = function(){   
+        if (!$scope.new_name ||!$scope.new_phone ) {
+            $scope.new_incomplete = true;
+        }else{
+            $scope.new_incomplete = false;
+        }   
+    }  
+//编辑
     $scope.editMarket = function($index){
-        console.log($scope.salesmans[$index])
+        console.log($scope.salesmans[$index]);
         $scope.name = $scope.salesmans[$index].name;
         $scope.phone = $scope.salesmans[$index].phone;
         $scope.remark = $scope.salesmans[$index].remark;
@@ -23,14 +31,23 @@ app.controller('manager2Ctrl', function ($scope,$http) {
             $scope.incomplete = false;
         }   
     }
+//删除
+    // $scope.confirmDle = function(){
+    //     alert()
+    // }
+    $scope.delMarket = function($index){
+        console.log($index);
+        console.log($scope.salesmans[$index].id);
+        $http.post("/crm/user/delete/"+$scope.salesmans[$index].id)
+        .success(function(result){
+            if(result.code == 0){
+                 $scope.salesmans.splice($index,1);
 
-    $scope.$watch('new_name',function(){$scope.test2();});
-    $scope.$watch('new_phone',function(){$scope.test2();});
-    $scope.test2 = function(){   
-        if (!$scope.new_name ||!$scope.new_phone ) {
-            $scope.new_incomplete = true;
-        }else{
-            $scope.new_incomplete = false;
-        }   
+            }else{
+                alert(msg)
+            }
+        })
     }
+
+
 });
